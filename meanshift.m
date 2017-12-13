9f2cda1789fb3a67c482032f34078c0cda3b0b29
[valkyrie7] <iamotley7@gmail.com>
2017-12-13 16:31:40 +0300

    Implemented a basic version of mean shift

--- meanshift.m
+++ meanshift.m
@@ -1,14 +1,34 @@
 % data : nxd, params: r, the size of the search window and t, threshold
-% criterion, c: center index
-function peak = meanshift(data, c, params)
+% criterion, i: center index
+function peak = meanshift(data, i, params)
     % center coordinates
-    center = data(c,:);
-    
-    while( ~is_coverged(center, mean_center) ) % change: they are not have to be equal
-        peak
-    end
-    peak = center;
+    center = data(i,:);
+	
+    r, t, mode = params;
+	n = size(data,1);															% check
+    mean_center = -100;															% check
+	list = []
+	if( mode == 'spectral')
+		while( true )
+			for( point=1:n ) 
+				if(	inwindow(center, point, r) )
+					list.append(point);
+				end
+			end
+			mean_center = mean(list);											% ? 
+			if (norm(center - mean_center) < t)
+				break;
+			end
+			center = mean_center
+		end
+	end
+	peak = center;
 end
 
-function is_coverged(center, mean_center)
+function in_window(x, y, r)
+	r1, g1, b1 = x;
+	r2, g2, b2 = y;
+	return if(	r1 - r2 < r/2 || r2 - r1 < r/2 ...
+				g1 - g2 < r/2 || g2 - g1 < r/2 ...
+				b1 - b2 < r/2 || b2 - b1 < r/2 );
 end
